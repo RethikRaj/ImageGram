46d173d319eabf73079439834557d08fe730b230
RethikRaj <rethikrajrr@gmail.com>
2025-03-31 15:47:29 +0530

    added aws config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,3 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+export const AWS_REGION = process.env.AWS_REGION;
