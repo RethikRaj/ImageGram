d3557a6ca978de127e5ab61e58e31d478a210e4e
RethikRaj <rethikrajrr@gmail.com>
2025-03-31 16:06:21 +0530

    added multer config and upload middleware

---
+++ src/config/multerConfig.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import s3 from "../config/awsConfig.js";
+import { AWS_BUCKET_NAME } from "./serverConfig.js";
+
+const storage = multerS3({
+    s3: s3,
+    bucket: AWS_BUCKET_NAME,    
+    key: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+        const fileName = `${file.fieldname}-${uniqueSuffix}.${file.mimetype.split("/")[1]}`;
+        cb(null, fileName); // null is the error, fileName is the key
+    }
+});
+
+export const s3uploader = multer({
+    storage: storage
+})
