2e800a146c5815dec209572cf5d36e7d85695d93
RethikRaj <rethikrajrr@gmail.com>
2025-03-31 10:31:25 +0530

    added user repository

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,33 @@
+import User from "../schema/user";
+
+export const createUser = async (username, email, password)=>{
+    try {
+        const newUser = await User.create({username, email, password});
+        // or 
+        // const user = new User({username, email, password});
+        // await user.save();
+
+        return newUser;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const findUserByEmail = async (email)=>{
+    try {
+        const user = await User.findOne({email});
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const findAllUsers = async ()=>{
+    try {
+        const users = await User.find();
+        return users;
+    } catch (error) {
+        console.log(error);
+    }
+}
