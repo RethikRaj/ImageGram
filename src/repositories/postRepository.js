cfef66fab24745c4b7847e38aba5110d7b2e0b37
RethikRaj <rethikrajrr@gmail.com>
2025-03-31 10:34:55 +0530

    added post repository

---
+++ src/repositories/postRepository.js
@@ -0,0 +1,37 @@
+import Post from '../models/Post.js';
+
+export const createPost = async (imageUrl, caption, userId) => {
+    try {
+        const newPost = await Post.create({ imageUrl, caption, userId });
+        return newPost;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const findPostById = async (postId) => {
+    try {
+        const post = await Post.findById(postId);
+        return post;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const findAllPosts = async () => {
+    try {
+        const posts = await Post.find();
+        return posts;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const deletePostById = async (postId) => {
+    try{
+        const deletedPost = await Post.findByIdAndDelete(postId);
+        return deletedPost;
+    }catch(error){
+        console.error(error);
+    }
+}
