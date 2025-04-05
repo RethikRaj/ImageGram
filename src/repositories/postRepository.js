fb3365e8c192a2d630235d7994933197bbe0996b
RethikRaj <rethikrajrr@gmail.com>
2025-04-05 18:43:59 +0530

    fixed bug in getting paginated posts

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -18,9 +18,9 @@ export const findPostById = async (postId) => {
     }
 }
 
-export const findAllPosts = async () => {
+export const findAllPosts = async (limit,offset) => {
     try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 }).skip(offset).limit(limit);
         return posts;
     } catch (error) {
         console.error(error);
